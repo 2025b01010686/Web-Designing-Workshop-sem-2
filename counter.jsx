27fabd1064c0d2a5ed37e726a666dc94266878f6
2025b01010686 <saanvi.25b01010686@abes.ac.in>
2026-05-21 14:02:25 +0530

    Experiment 8

---
+++ counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function Counter(){
+    const[count, setCount] = useState(0)
+
+    const increment = () => setCount(count + 1)
+    const decrement = () => setCount(count - 1)
+    const reset = () => setCount(0)
+
+    return (
+        <div className="container">
+            <div className="title">React Counter Applicattion</div>
+            <h1 className="display">{count}</h1>
+        </div>
+    )
+}
